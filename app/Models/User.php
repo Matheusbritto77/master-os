fe172d1eae0bf2a08adf6d8bc6f71bf8ed2cf415
nortan25 <britto190499@gmail.com>
2025-05-15 15:27:58 -0300

    HJ

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,17 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // Importa o namespace correto
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmailContract
+class User extends Authenticatable // Remova "implements MustVerifyEmailContract"
 {
-    use HasApiTokens, HasFactory, Notifiable; // Certifique-se de usar todas as traits necessárias
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -26,23 +21,13 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'revendedor_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed', // Caso esteja utilizando hashing de senhas no Laravel 10+
+        'password' => 'hashed',
     ];
 }
